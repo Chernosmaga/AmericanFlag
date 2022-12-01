7483c9481d675b413ccfea9c2b75c5b55df829eb
Chernosmaga <117843513+Chernosmaga@users.noreply.github.com>
2022-12-01 23:57:02 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String a = "*";
+        String b = "=";
+
+        for (int z = 1; z <= 9; z++) {
+            for (int i = 1; i <= 6; i++) {
+                System.out.print(a);
+            }
+            for (int j = 1; j <= 34; j++) {
+                    System.out.print(b);
+                }
+                System.out.println();
+            }
+
+        for (int k = 1; k <= 40; k++) {
+            System.out.print(b);
+            for (int l = 1; l <= 6; l++);
+        }
+        System.out.println();
+        }
     }
-}
